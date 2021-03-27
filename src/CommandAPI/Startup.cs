4d5c24ed3e0396077e94ad497ebe9414f0e0589f
Chris Berger <cberger@turnpack.com>
2021-03-27 15:10:59 -0700

    added secrets.json

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace CommandAPI
 {
@@ -24,8 +25,14 @@ namespace CommandAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var builder = new NpgsqlConnectionStringBuilder();
+            builder.ConnectionString = 
+                Configuration.GetConnectionString("PostgreSQLConnection");
+            builder.Username = Configuration["UserID"];
+            builder.Password = Configuration["Password"];
+            
             services.AddDbContext<CommandContext>(opt => opt.UseNpgsql
-                (Configuration.GetConnectionString("PostgreSqlConnection")));
+                (builder.ConnectionString));
 
             services.AddControllers();
             services.AddScoped<ICommandAPIRepo,SqlCommandAPIRepo>();
