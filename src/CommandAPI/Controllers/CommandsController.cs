cf1872d70de3f1ee22bd94fc7c36bd43d069a3d8
Chris Berger <cberger@turnpack.com>
2021-03-27 13:11:54 -0700

    DbContext

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Data;
+using CommandAPI.Models;
 
 namespace CommandAPI.Controller
 {
@@ -7,11 +9,33 @@ namespace CommandAPI.Controller
     [ApiController]
     public class CommandsController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        private readonly ICommandAPIRepo _repository;
+
+        public CommandsController(ICommandAPIRepo repository)
         {
-            return new string[] {"this","is","hard","coded"};
+            _repository = repository;
+            
         }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAllCommands();
+            return Ok(commandItems);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            if (commandItem==null)
+            {
+                return NotFound();
+            }
+            return Ok(commandItem);
+        }
+
+
     }
 
 }
